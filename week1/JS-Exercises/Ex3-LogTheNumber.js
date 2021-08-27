80cd31201c94b032446b03db09e88584b98156f4
Marzieh <mar.31069@gmail.com>
2021-08-27 14:21:59 +0430

    Completed Exercise 3: Log the number

---
+++ week1/JS-Exercises/Ex3-LogTheNumber.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let numberX;
+
+console.log("The value of X is 'undefined' :");
+console.log(typeof(numberX));
+console.log(numberX);
+
+numberX = 3;
+
+console.log("The value of X is 'number' :");
+console.log(typeof(numberX));
+console.log(numberX);
