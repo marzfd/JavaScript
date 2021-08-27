7a4a7aef53a34d3280ed7e7d5f7f541e1b9e1124
Marzieh <mar.31069@gmail.com>
2021-08-27 13:48:57 +0430

    Completed JS Exercise 1: Hello world!

---
+++ week1/JS-Exercises/Ex1-HelloWorld.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// the message Hello world! in 10 different languages.
+
+console.log("Hello, World!") // English
+console.log("سلام، دنیا!") // Persian
+console.log("Hola, Mundo!") // Spanish
+console.log("नमस्ते, दुनिया!") // Hindi
+console.log("Halló, heimur!") // Icelandic 
+console.log("Hàlo, a Shaoghail!") // Scottish Gaelic
+console.log("Halo, Dunia!") // Indonesian
+console.log("Привет, мир!") // Russian
+console.log("salve, mundi!") // Latin
+console.log("Hej, världen!") // Swedish
