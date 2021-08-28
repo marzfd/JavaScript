cd056831b2466872201af9ab5c31bd2d01a04d05
Marzieh <mar.31069@gmail.com>
2021-08-28 12:37:31 +0430

    Completed JS Exercise 4: Log the string

---
+++ week1/JS-Exercises/Ex4-LogTheString.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let myString = "This is Marzieh Fadaee.";
+
+console.log("The value of myString is 'string':");
+console.log(myString);
+
+myString = "Coding !"
+
+console.log("I'm Passionate about :");
+console.log(myString);
