4c7632e6f09300898217ff069330bfbfd33ffba4
Marzieh <mar.31069@gmail.com>
2021-08-28 20:14:54 +0430

    Completed JS Exercise 9: Log the remainder

---
+++ week1/JS-Exercises/Ex9-LogTheRemainder.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let x = 7;
+console.log(x % 3); //The answer is "1".
+
+let y = 21;
+console.log(y % 4); //The answer is "1".
+
+let z = 13;
+console.log(z % 2); //The answer is "1".
