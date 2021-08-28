c692540e32a8e4bb74c9744c4a0756537a702ac7
Marzieh <mar.31069@gmail.com>
2021-08-28 18:58:41 +0430

    Completed JS Exercise 6: Log an array of animals

---
+++ week1/JS-Exercises/Ex6-LogAnArrayOfAnimals.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let items = [];
+
+console.log("The value of 'items' is 'Array'.");
+console.log("items = ", items);
+
+let listOfAnimals = ['Horse', 'Panda', 'Cat'];
+
+console.log('My favorite animals are: ', listOfAnimals);
+
+listOfAnimals.push('Piglet');
+
+console.log(listOfAnimals);
