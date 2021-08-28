fe92481783bd159c710cfd2cc4e1cca377476ff5
Marzieh <mar.31069@gmail.com>
2021-08-28 20:02:37 +0430

    fixed errors

--- week1/JS-Exercises/Ex3-LogTheNumber.js
+++ week1/JS-Exercises/Ex3-LogTheNumber.js
@@ -3,11 +3,9 @@
 let numberX;
 
 console.log("The value of X is 'undefined' :");
-console.log(typeof(numberX));
 console.log(numberX);
 
 numberX = 3;
 
-console.log("The value of X is 'number' :");
-console.log(typeof(numberX));
+console.log("The value of X is '3' :");
 console.log(numberX);
