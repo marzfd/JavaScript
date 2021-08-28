7b63dc1e6d93a3bec240078e0a87462180b9bf22
Marzieh <mar.31069@gmail.com>
2021-08-28 12:49:03 +0430

    Completed JS Exercise 5: Round a number and log it

---
+++ week1/JS-Exercises/Ex5-RoundANumberAndLogIt.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let z = 7.25;
+
+console.log('z = ',z);
+
+let a = Math.round(z);
+
+console.log('a = ',a);
+
+if (z >= a) {
+  let b = z;
+  console.log('z is greater :', b);
+
+
+} else {
+  b = a
+  console.log('a is greater :',b);
+}
