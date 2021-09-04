bda8205e26cb00387da64645216e95d97a9ee1c3
Marzieh <mar.31069@gmail.com>
2021-09-04 20:04:31 +0430

    Completed JS Exercise 2: Dog years

---
+++ week3/JS-Exercises/ex2-dogYears.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function calculateDogAge(age) {
+ let dogAge = parseFloat(age) * (7);
+ return console.log(`Your doggie is ${dogAge} years old in dog years!`);
+}
+
+calculateDogAge('1');
+calculateDogAge('2');
+calculateDogAge('3');
