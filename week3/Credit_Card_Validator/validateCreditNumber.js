317cf4098d8dd3429399d1765dd4a63ca771b889
Marzieh <mar.31069@gmail.com>
2021-09-09 15:26:48 +0430

    Define a function by JavaScript

---
+++ week3/Credit_Card_Validator/validateCreditNumber.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const validateCreditNumber = function(str) {
+
+ // Are all input numbers the same ??
+  const isSameNumber = function(s) {
+    for (let i = 0; i < (s.length); i++) {
+      if (s.charAt(i) === s.charAt(i + 1)) {
+        return true;
+      } else {
+        return false
+      }
+    };
+    return true
+  };
+
+  // Add the numbers in the input to get the sum !!
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    sum = sum + parseFloat(str.charAt(i));
+  };
+
+  // Validation of Credit Card Numbers :
+  if (str.length !== 16) {
+    console.log(`"Invalid! The input ${str} must be 16 characters!"`);
+
+  } else if (isNaN(str)) {
+    console.log(`"Invalid! The input ${str} should contain only numbers!"`);
+
+  } else if (parseFloat(str) % 2 !== 0) {
+    console.log(`"Invalid! The last number of input ${str} should be even!"`);
+
+  } else if (sum < 16) {
+    console.log(`"Invalid! The sum of all numbers in input ${str} should be equal to or greater than 16!"`);
+  
+  } else if (isSameNumber(str)) {
+    console.log(`"Invalid! The input ${str} should contain at least 2 different types of numbers!"`);
+
+  } else {
+    console.log(`"Success! The input ${str} is a valid credit card number!"`);
+  };
+};
+
+validateCreditNumber('92332119011112');
+validateCreditNumber('a92332119c011112');
+validateCreditNumber('4444444444444444');
+validateCreditNumber('6666666666666661');
+validateCreditNumber('1111111111111110');
+validateCreditNumber('7999977788880000');
+validateCreditNumber('1666666666666666');
