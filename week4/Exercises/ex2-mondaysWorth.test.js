2f54a7202026ecbfd3bb19a7774def0a8ffc5c67
Marzieh <mar.31069@gmail.com>
2021-09-11 21:53:57 +0430

    Completed Exercise 2: Whats your Monday worth

---
+++ week4/Exercises/ex2-mondaysWorth.test.js
@@ -0,0 +1,45 @@
+const mondayTasks = [
+  {
+    name: 'Daily standup',
+    duration: 30, // specified in minutes
+  },
+  {
+    name: 'Feature discussion',
+    duration: 120,
+  },
+  {
+    name: 'Development time',
+    duration: 240,
+  },
+  {
+    name: 'Talk to different members from the product team',
+    duration: 60,
+  },
+];
+
+const hourlyRate = 25;
+
+function computeEarnings(tasks, rate) {
+  
+  const totalEarnings = tasks
+    .map(tasks => (tasks.duration * rate) /60)
+    .reduce((a, b) => a + b)
+
+  return `€${totalEarnings.toFixed(2)}`;
+  }
+
+
+// jest test
+describe('computeEarnings', () => {
+  test('should take two parameters', () => {
+    // The `.length` property indicates the number of parameters expected by
+    // the function.
+    expect(computeEarnings).toHaveLength(2);
+  });
+
+  test('should compute the earnings as a formatted Euro amount', () => {
+    const result = computeEarnings(mondayTasks, hourlyRate);
+    const expected = '€187.50';
+    expect(result).toBe(expected);
+  });
+});
